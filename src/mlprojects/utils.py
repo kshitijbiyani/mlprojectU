50f183e7a1e6152e91b7c736e1c4da7e0b55693b
Kshitij Biyani <kbiyani2@gmail.com>
2025-05-18 01:55:22 +0530

    Code with hyper paramters

--- src/mlprojects/utils.py
+++ src/mlprojects/utils.py
@@ -5,7 +5,7 @@ import numpy as np
 import pandas as pd
 import dill
 from sklearn.metrics import r2_score
-
+from sklearn.model_selection import GridSearchCV
 
 import os
 import dill
@@ -33,13 +33,23 @@ def save_object(file_path, obj):
 #     with open(file_path, "wb") as file_obj:
 #         dill.dump(obj,file_path)
         
-def evaluate_model(X_train,y_train,X_test,y_test,models):
+def evaluate_model(X_train,y_train,X_test,y_test,models,params):
     try:
         report={}
         for i in range(len(list(models))):
             model=list(models.values())[i]
             
+            para=params[list(models.keys())[i]]
+
+            gs = GridSearchCV(model,para,cv=3)
+            gs.fit(X_train,y_train)
+
+            model.set_params(**gs.best_params_)
             model.fit(X_train,y_train)
+
+            
+            
+            # model.fit(X_train,y_train)
             
             y_train_pred=model.predict(X_train)
             
